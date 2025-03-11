734cc089006d4966d4c857366b6a38f9f0616f02
yoyosource <yoyosource@nidido.de>
2025-03-11 15:01:47 +0100

    Add a todo

--- src/main/java/de/yoyosource/streamable3/Test.java
+++ src/main/java/de/yoyosource/streamable3/Test.java
@@ -3,6 +3,7 @@ package de.yoyosource.streamable3;
 public class Test {
 
     public static void main(String[] args) {
+        // TODO: Add Exception handling on Exception in Streamable Step
         Streamable.of("Hello World")
                 .map(s -> "1 " + s)
                 .findFirst()
