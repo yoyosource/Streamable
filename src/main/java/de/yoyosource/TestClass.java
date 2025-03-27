b9afbad2484eeec403ed23083725af751e4a0e02
yoyosource <yoyosource@nidido.de>
2025-03-27 09:54:04 +0100

    Add TestClassFile

---
+++ src/main/java/de/yoyosource/TestClass.java
@@ -0,0 +1,10 @@
+package de.yoyosource;
+
+public class TestClass {
+
+    public static void run(Iterable iterable) {
+        for (Object o : iterable) {
+            System.out.println(o);
+        }
+    }
+}
