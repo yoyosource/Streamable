9006006e50c2101f4ef55052bae1e32bb13831b9
yoyosource <yoyosource@nidido.de>
2025-03-27 17:38:22 +0100

    Add TryingStream.tryIt with endOfException Add TryedStream.tryIt with endOfException

--- src/main/java/de/yoyosource/streamable/streams/TryingStream.java
+++ src/main/java/de/yoyosource/streamable/streams/TryingStream.java
@@ -1,9 +1,9 @@
 package de.yoyosource.streamable.streams;
 
 import de.yoyosource.streamable.FunctionWithException;
-import de.yoyosource.streamable.Try;
 import de.yoyosource.streamable.Streamable;
 import de.yoyosource.streamable.StreamableGatherer;
+import de.yoyosource.streamable.Try;
 
 import java.util.function.Consumer;
 
@@ -20,13 +20,21 @@ public interface TryingStream<T> extends Streamable<TryingStream<T>, T> {
     }
 
     default <R, E extends Throwable> TryedStream<R, E> tryIt(FunctionWithException<T, R, E> functionWithException) {
+        return tryIt(functionWithException, false);
+    }
+
+    default <R, E extends Throwable> TryedStream<R, E> tryIt(FunctionWithException<T, R, E> functionWithException, boolean endOnException) {
         return gather(new StreamableGatherer.Simple<T, Try<R, E>>() {
             @Override
             public boolean integrate(long index, T element, Consumer<? super Try<R, E>> next) {
                 try {
                     next.accept(Try.Success(functionWithException.apply(element)));
                 } catch (Throwable e) {
-                    next.accept(Try.Failure((E) e));
+                    if (endOnException) {
+                        return true;
+                    } else {
+                        next.accept(Try.Failure((E) e));
+                    }
                 }
                 return false;
             }
