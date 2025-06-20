6c926dfed92a60f254dbd31232f73ddc6af4e20c
yoyosource <yoyosource@nidido.de>
2025-06-20 10:04:52 +0200

    Add AllElementsTest

--- src/test/java/de/yoyosource/advancedstream/AllElementsTest.java
+++ src/test/java/de/yoyosource/advancedstream/AllElementsTest.java
@@ -1,4 +1,37 @@
 package de.yoyosource.advancedstream;
 
+import de.yoyosource.streamable.Streamable;
+import de.yoyosource.streamable.streams.AdvancedStream;
+import de.yoyosource.streamable.streams.JavaStream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 class AllElementsTest {
+
+    @Test
+    void allElementsTest() {
+        List<List<Integer>> list = Streamable.of(1, 2, 3)
+                .as(AdvancedStream.AdvancedStream())
+                .allElements()
+                .as(JavaStream.JavaStream())
+                .toList();
+        Assertions.assertEquals(1, list.size());
+        Assertions.assertEquals(3, list.get(0).size());
+        Assertions.assertEquals(1, list.get(0).get(0));
+        Assertions.assertEquals(2, list.get(0).get(1));
+        Assertions.assertEquals(3, list.get(0).get(2));
+    }
+
+    @Test
+    void allElementsTestNoElements() {
+        List<List<Integer>> list = Streamable.<Integer>of()
+                .as(AdvancedStream.AdvancedStream())
+                .allElements()
+                .as(JavaStream.JavaStream())
+                .toList();
+        Assertions.assertEquals(1, list.size());
+        Assertions.assertEquals(0, list.get(0).size());
+    }
 }
