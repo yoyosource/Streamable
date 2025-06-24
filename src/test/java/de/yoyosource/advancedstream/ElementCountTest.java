a961c4b9ea3ca040fd666e8169f48786a708043b
yoyosource <yoyosource@nidido.de>
2025-06-24 07:48:48 +0200

    Add ElementCountTest

--- src/test/java/de/yoyosource/advancedstream/ElementCountTest.java
+++ src/test/java/de/yoyosource/advancedstream/ElementCountTest.java
@@ -1,4 +1,28 @@
 package de.yoyosource.advancedstream;
 
+import de.yoyosource.streamable.Streamable;
+import de.yoyosource.streamable.streams.AdvancedStream;
+import de.yoyosource.streamable.streams.JavaStream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 class ElementCountTest {
+
+    @Test
+    void testElementCount() {
+        List<Integer> list = Streamable.of(1, 2, 3)
+                .as(AdvancedStream.AdvancedStream())
+                .elementCount(count -> {
+                    Assertions.assertEquals(3, count);
+                })
+                .as(JavaStream.JavaStream())
+                .collect(Collectors.toList());
+        Assertions.assertEquals(3, list.size());
+        Assertions.assertEquals(1, list.get(0));
+        Assertions.assertEquals(2, list.get(1));
+        Assertions.assertEquals(3, list.get(2));
+    }
 }
