1f8a3fce4b87a08f1c716fe50ce4d6f72dc82c32
yoyosource <yoyosource@nidido.de>
2025-09-15 08:50:45 +0200

    Add parallel tests

--- src/test/java/de/yoyosource/streamable/CloseTest.java
+++ src/test/java/de/yoyosource/streamable/CloseTest.java
@@ -3,6 +3,7 @@ package de.yoyosource.streamable;
 import de.yoyosource.streamable.streams.AdvancedStream;
 import de.yoyosource.streamable.streams.JavaStream;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -10,54 +11,121 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 class CloseTest {
 
-    @Test
-    void testClose() {
-        AtomicBoolean closed = new AtomicBoolean(false);
-        Streamable.of(1, 2, 3)
-                .onClose(() -> closed.set(true))
-                .forEach((i) -> {
-                    if (closed.get()) Assertions.fail("Streamable.close() should not be called before last Element");
-                });
-        Assertions.assertTrue(closed.get());
-    }
-
-    @Test
-    void testManuallyClose() {
-        AtomicBoolean closed = new AtomicBoolean(false);
-        JavaStream<Integer> stream = Streamable.of(1, 2, 3)
-                .onClose(() -> closed.set(true));
-        stream.close();
-        Assertions.assertTrue(closed.get());
-    }
-
-    @Test
-    void testCloseWithConcat() {
-        AtomicInteger closed = new AtomicInteger();
-        Streamable.of(1, 2, 3)
-                .onClose(() -> closed.incrementAndGet())
-                .as(AdvancedStream.AdvancedStream())
-                .concat(Streamable.of(4, 5, 6).onClose(() -> closed.incrementAndGet()))
-                .forEach(i -> {});
-        Assertions.assertEquals(2, closed.get());
-    }
-
-    @Test
-    void testCloseWithZip() {
-        AtomicBoolean closed = new AtomicBoolean(false);
-        Streamable.of(1, 2, 3)
-                .as(AdvancedStream.AdvancedStream())
-                .zip(Streamable.of(4, 5, 6).onClose(() -> closed.set(true)))
-                .forEach((i) -> {});
-        Assertions.assertTrue(closed.get());
-    }
-
-    @Test
-    void testCloseTryWithResources() {
-        AtomicBoolean closed = new AtomicBoolean(false);
-        try (JavaStream<Integer> stream = Streamable.of(1, 2, 3).onClose(() -> {
-            closed.set(true);
-        })) {
+    @Nested
+    class Sequential {
+        @Test
+        void testClose() {
+            AtomicBoolean closed = new AtomicBoolean(false);
+            Streamable.of(1, 2, 3)
+                    .onClose(() -> closed.set(true))
+                    .forEach((i) -> {
+                        if (closed.get())
+                            Assertions.fail("Streamable.close() should not be called before last Element");
+                    });
+            Assertions.assertTrue(closed.get());
         }
-        Assertions.assertTrue(closed.get());
+
+        @Test
+        void testManuallyClose() {
+            AtomicBoolean closed = new AtomicBoolean(false);
+            JavaStream<Integer> stream = Streamable.of(1, 2, 3)
+                    .onClose(() -> closed.set(true));
+            stream.close();
+            Assertions.assertTrue(closed.get());
+        }
+
+        @Test
+        void testCloseWithConcat() {
+            AtomicInteger closed = new AtomicInteger();
+            Streamable.of(1, 2, 3)
+                    .onClose(() -> closed.incrementAndGet())
+                    .as(AdvancedStream.AdvancedStream())
+                    .concat(Streamable.of(4, 5, 6).onClose(() -> closed.incrementAndGet()))
+                    .forEach(i -> {
+                    });
+            Assertions.assertEquals(2, closed.get());
+        }
+
+        @Test
+        void testCloseWithZip() {
+            AtomicBoolean closed = new AtomicBoolean(false);
+            Streamable.of(1, 2, 3)
+                    .as(AdvancedStream.AdvancedStream())
+                    .zip(Streamable.of(4, 5, 6).onClose(() -> closed.set(true)))
+                    .forEach((i) -> {
+                    });
+            Assertions.assertTrue(closed.get());
+        }
+
+        @Test
+        void testCloseTryWithResources() {
+            AtomicBoolean closed = new AtomicBoolean(false);
+            try (JavaStream<Integer> stream = Streamable.of(1, 2, 3).onClose(() -> {
+                closed.set(true);
+            })) {
+            }
+            Assertions.assertTrue(closed.get());
+        }
+    }
+
+    @Nested
+    class Parallel {
+        @Test
+        void testClose() {
+            AtomicBoolean closed = new AtomicBoolean(false);
+            Streamable.of(1, 2, 3)
+                    .parallel(3)
+                    .onClose(() -> closed.set(true))
+                    .forEach((i) -> {
+                        if (closed.get())
+                            Assertions.fail("Streamable.close() should not be called before last Element");
+                    });
+            Assertions.assertTrue(closed.get());
+        }
+
+        @Test
+        void testManuallyClose() {
+            AtomicBoolean closed = new AtomicBoolean(false);
+            JavaStream<Integer> stream = Streamable.of(1, 2, 3)
+                    .parallel(3)
+                    .onClose(() -> closed.set(true));
+            stream.close();
+            Assertions.assertTrue(closed.get());
+        }
+
+        @Test
+        void testCloseWithConcat() {
+            AtomicInteger closed = new AtomicInteger();
+            Streamable.of(1, 2, 3)
+                    .parallel(3)
+                    .onClose(() -> closed.incrementAndGet())
+                    .as(AdvancedStream.AdvancedStream())
+                    .concat(Streamable.of(4, 5, 6).onClose(() -> closed.incrementAndGet()))
+                    .forEach(i -> {
+                    });
+            Assertions.assertEquals(2, closed.get());
+        }
+
+        @Test
+        void testCloseWithZip() {
+            AtomicBoolean closed = new AtomicBoolean(false);
+            Streamable.of(1, 2, 3)
+                    .parallel(3)
+                    .as(AdvancedStream.AdvancedStream())
+                    .zip(Streamable.of(4, 5, 6).onClose(() -> closed.set(true)))
+                    .forEach((i) -> {
+                    });
+            Assertions.assertTrue(closed.get());
+        }
+
+        @Test
+        void testCloseTryWithResources() {
+            AtomicBoolean closed = new AtomicBoolean(false);
+            try (JavaStream<Integer> stream = Streamable.of(1, 2, 3).parallel(3).onClose(() -> {
+                closed.set(true);
+            })) {
+            }
+            Assertions.assertTrue(closed.get());
+        }
     }
 }
--- src/test/java/de/yoyosource/streamable/ConstructorTest.java
+++ src/test/java/de/yoyosource/streamable/ConstructorTest.java
@@ -1,6 +1,7 @@
 package de.yoyosource.streamable;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.Iterator;
@@ -10,155 +11,327 @@ import java.util.stream.Stream;
 
 class ConstructorTest {
 
-    @Test
-    void emptyTest() {
-        List list = Streamable.empty()
-                .toList();
-        Assertions.assertEquals(0, list.size());
+    @Nested
+    class Sequential {
+        @Test
+        void emptyTest() {
+            List list = Streamable.empty()
+                    .toList();
+            Assertions.assertEquals(0, list.size());
+        }
+
+        @Test
+        void ofTest() {
+            List<Integer> list = Streamable.of(1)
+                    .toList();
+            Assertions.assertEquals(1, list.size());
+            Assertions.assertEquals(1, list.get(0));
+        }
+
+        @Test
+        void ofTestWithNull() {
+            List list = Streamable.of((Integer) null)
+                    .toList();
+            Assertions.assertEquals(1, list.size());
+            Assertions.assertNull(list.get(0));
+        }
+
+        @Test
+        void ofNullableTest() {
+            List<Integer> list = Streamable.ofNullable(1)
+                    .toList();
+            Assertions.assertEquals(1, list.size());
+            Assertions.assertEquals(1, list.get(0));
+        }
+
+        @Test
+        void ofNullableTestWithNull() {
+            List list = Streamable.ofNullable(null)
+                    .toList();
+            Assertions.assertEquals(0, list.size());
+        }
+
+        @Test
+        void ofTestVarArgs() {
+            List<Integer> list = Streamable.of(1, 2, 3)
+                    .toList();
+            Assertions.assertEquals(3, list.size());
+            Assertions.assertEquals(1, list.get(0));
+            Assertions.assertEquals(2, list.get(1));
+            Assertions.assertEquals(3, list.get(2));
+        }
+
+        @Test
+        void ofTestVarArgsEmpty() {
+            List list = Streamable.of()
+                    .toList();
+            Assertions.assertEquals(0, list.size());
+        }
+
+        @Test
+        void iterateTest() {
+            List<Integer> list = Streamable.iterate(0, i -> i + 1)
+                    .limit(5)
+                    .toList();
+            Assertions.assertEquals(5, list.size());
+            Assertions.assertEquals(0, list.get(0));
+            Assertions.assertEquals(1, list.get(1));
+            Assertions.assertEquals(2, list.get(2));
+            Assertions.assertEquals(3, list.get(3));
+            Assertions.assertEquals(4, list.get(4));
+        }
+
+        @Test
+        void iterateTestWithLimit() {
+            List<Integer> list = Streamable.iterate(0, i -> i < 5, i -> i + 1)
+                    .toList();
+            Assertions.assertEquals(5, list.size());
+            Assertions.assertEquals(0, list.get(0));
+            Assertions.assertEquals(1, list.get(1));
+            Assertions.assertEquals(2, list.get(2));
+            Assertions.assertEquals(3, list.get(3));
+            Assertions.assertEquals(4, list.get(4));
+        }
+
+        @Test
+        void generateTest() {
+            List<Integer> list = Streamable.generate(() -> 0)
+                    .limit(5)
+                    .toList();
+            Assertions.assertEquals(5, list.size());
+            Assertions.assertEquals(0, list.get(0));
+            Assertions.assertEquals(0, list.get(1));
+            Assertions.assertEquals(0, list.get(2));
+            Assertions.assertEquals(0, list.get(3));
+            Assertions.assertEquals(0, list.get(4));
+        }
+
+        @Test
+        void fromTestStream() {
+            List<Integer> list = Streamable.from(Stream.of(1, 2, 3))
+                    .toList();
+            Assertions.assertEquals(3, list.size());
+            Assertions.assertEquals(1, list.get(0));
+            Assertions.assertEquals(2, list.get(1));
+            Assertions.assertEquals(3, list.get(2));
+        }
+
+        @Test
+        void fromTestIterableWithStreamable() {
+            List<Integer> list = Streamable.from(Streamable.of(1, 2, 3))
+                    .toList();
+            Assertions.assertEquals(3, list.size());
+            Assertions.assertEquals(1, list.get(0));
+            Assertions.assertEquals(2, list.get(1));
+            Assertions.assertEquals(3, list.get(2));
+        }
+
+        @Test
+        void fromTestIterableWithList() {
+            List<Integer> list = Streamable.from(List.of(1, 2, 3))
+                    .toList();
+            Assertions.assertEquals(3, list.size());
+            Assertions.assertEquals(1, list.get(0));
+            Assertions.assertEquals(2, list.get(1));
+            Assertions.assertEquals(3, list.get(2));
+        }
+
+        @Test
+        void fromTestIterableWithSet() {
+            List<Integer> list = Streamable.from(Set.of(1, 2, 3))
+                    .toList();
+            Assertions.assertEquals(3, list.size());
+            Assertions.assertTrue(list.contains(1));
+            Assertions.assertTrue(list.contains(2));
+            Assertions.assertTrue(list.contains(3));
+        }
+
+        @Test
+        void fromTestIterator() {
+            List<Integer> list = Streamable.from(new Iterator<Integer>() {
+                @Override
+                public boolean hasNext() {
+                    return true;
+                }
+
+                @Override
+                public Integer next() {
+                    return 0;
+                }
+            }).limit(3).toList();
+            Assertions.assertEquals(3, list.size());
+            Assertions.assertEquals(0, list.get(0));
+            Assertions.assertEquals(0, list.get(1));
+            Assertions.assertEquals(0, list.get(2));
+        }
     }
 
-    @Test
-    void ofTest() {
-        List<Integer> list = Streamable.of(1)
-                .toList();
-        Assertions.assertEquals(1, list.size());
-        Assertions.assertEquals(1, list.get(0));
-    }
+    @Nested
+    class Parallel {
+        @Test
+        void emptyTest() {
+            List list = Streamable.empty()
+                    .parallel(3)
+                    .toList();
+            Assertions.assertEquals(0, list.size());
+        }
 
-    @Test
-    void ofTestWithNull() {
-        List list = Streamable.of((Integer) null)
-                .toList();
-        Assertions.assertEquals(1, list.size());
-        Assertions.assertNull(list.get(0));
-    }
+        @Test
+        void ofTest() {
+            List<Integer> list = Streamable.of(1)
+                    .parallel(3)
+                    .toList();
+            Assertions.assertEquals(1, list.size());
+            Assertions.assertEquals(1, list.get(0));
+        }
 
-    @Test
-    void ofNullableTest() {
-        List<Integer> list = Streamable.ofNullable(1)
-                .toList();
-        Assertions.assertEquals(1, list.size());
-        Assertions.assertEquals(1, list.get(0));
-    }
+        @Test
+        void ofTestWithNull() {
+            List list = Streamable.of((Integer) null)
+                    .parallel(3)
+                    .toList();
+            Assertions.assertEquals(1, list.size());
+            Assertions.assertNull(list.get(0));
+        }
 
-    @Test
-    void ofNullableTestWithNull() {
-        List list = Streamable.ofNullable(null)
-                .toList();
-        Assertions.assertEquals(0, list.size());
-    }
+        @Test
+        void ofNullableTest() {
+            List<Integer> list = Streamable.ofNullable(1)
+                    .parallel(3)
+                    .toList();
+            Assertions.assertEquals(1, list.size());
+            Assertions.assertEquals(1, list.get(0));
+        }
 
-    @Test
-    void ofTestVarArgs() {
-        List<Integer> list = Streamable.of(1, 2, 3)
-                .toList();
-        Assertions.assertEquals(3, list.size());
-        Assertions.assertEquals(1, list.get(0));
-        Assertions.assertEquals(2, list.get(1));
-        Assertions.assertEquals(3, list.get(2));
-    }
+        @Test
+        void ofNullableTestWithNull() {
+            List list = Streamable.ofNullable(null)
+                    .parallel(3)
+                    .toList();
+            Assertions.assertEquals(0, list.size());
+        }
 
-    @Test
-    void ofTestVarArgsEmpty() {
-        List list = Streamable.of()
-                .toList();
-        Assertions.assertEquals(0, list.size());
-    }
+        @Test
+        void ofTestVarArgs() {
+            List<Integer> list = Streamable.of(1, 2, 3)
+                    .parallel(3)
+                    .toList();
+            Assertions.assertEquals(3, list.size());
+            Assertions.assertEquals(1, list.get(0));
+            Assertions.assertEquals(2, list.get(1));
+            Assertions.assertEquals(3, list.get(2));
+        }
 
-    @Test
-    void iterateTest() {
-        List<Integer> list = Streamable.iterate(0, i -> i + 1)
-                .limit(5)
-                .toList();
-        Assertions.assertEquals(5, list.size());
-        Assertions.assertEquals(0, list.get(0));
-        Assertions.assertEquals(1, list.get(1));
-        Assertions.assertEquals(2, list.get(2));
-        Assertions.assertEquals(3, list.get(3));
-        Assertions.assertEquals(4, list.get(4));
-    }
+        @Test
+        void ofTestVarArgsEmpty() {
+            List list = Streamable.of()
+                    .parallel(3)
+                    .toList();
+            Assertions.assertEquals(0, list.size());
+        }
 
-    @Test
-    void iterateTestWithLimit() {
-        List<Integer> list = Streamable.iterate(0, i -> i < 5, i -> i + 1)
-                .toList();
-        Assertions.assertEquals(5, list.size());
-        Assertions.assertEquals(0, list.get(0));
-        Assertions.assertEquals(1, list.get(1));
-        Assertions.assertEquals(2, list.get(2));
-        Assertions.assertEquals(3, list.get(3));
-        Assertions.assertEquals(4, list.get(4));
-    }
+        @Test
+        void iterateTest() {
+            List<Integer> list = Streamable.iterate(0, i -> i + 1)
+                    .parallel(3)
+                    .limit(5)
+                    .toList();
+            Assertions.assertEquals(5, list.size());
+            Assertions.assertEquals(0, list.get(0));
+            Assertions.assertEquals(1, list.get(1));
+            Assertions.assertEquals(2, list.get(2));
+            Assertions.assertEquals(3, list.get(3));
+            Assertions.assertEquals(4, list.get(4));
+        }
 
-    @Test
-    void generateTest() {
-        List<Integer> list = Streamable.generate(() -> 0)
-                .limit(5)
-                .toList();
-        Assertions.assertEquals(5, list.size());
-        Assertions.assertEquals(0, list.get(0));
-        Assertions.assertEquals(0, list.get(1));
-        Assertions.assertEquals(0, list.get(2));
-        Assertions.assertEquals(0, list.get(3));
-        Assertions.assertEquals(0, list.get(4));
-    }
+        @Test
+        void iterateTestWithLimit() {
+            List<Integer> list = Streamable.iterate(0, i -> i < 5, i -> i + 1)
+                    .parallel(3)
+                    .toList();
+            Assertions.assertEquals(5, list.size());
+            Assertions.assertEquals(0, list.get(0));
+            Assertions.assertEquals(1, list.get(1));
+            Assertions.assertEquals(2, list.get(2));
+            Assertions.assertEquals(3, list.get(3));
+            Assertions.assertEquals(4, list.get(4));
+        }
 
-    @Test
-    void fromTestStream() {
-        List<Integer> list = Streamable.from(Stream.of(1, 2, 3))
-                .toList();
-        Assertions.assertEquals(3, list.size());
-        Assertions.assertEquals(1, list.get(0));
-        Assertions.assertEquals(2, list.get(1));
-        Assertions.assertEquals(3, list.get(2));
-    }
+        @Test
+        void generateTest() {
+            List<Integer> list = Streamable.generate(() -> 0)
+                    .parallel(3)
+                    .limit(5)
+                    .toList();
+            Assertions.assertEquals(5, list.size());
+            Assertions.assertEquals(0, list.get(0));
+            Assertions.assertEquals(0, list.get(1));
+            Assertions.assertEquals(0, list.get(2));
+            Assertions.assertEquals(0, list.get(3));
+            Assertions.assertEquals(0, list.get(4));
+        }
 
-    @Test
-    void fromTestIterableWithStreamable() {
-        List<Integer> list = Streamable.from(Streamable.of(1, 2, 3))
-                .toList();
-        Assertions.assertEquals(3, list.size());
-        Assertions.assertEquals(1, list.get(0));
-        Assertions.assertEquals(2, list.get(1));
-        Assertions.assertEquals(3, list.get(2));
-    }
+        @Test
+        void fromTestStream() {
+            List<Integer> list = Streamable.from(Stream.of(1, 2, 3))
+                    .parallel(3)
+                    .toList();
+            Assertions.assertEquals(3, list.size());
+            Assertions.assertEquals(1, list.get(0));
+            Assertions.assertEquals(2, list.get(1));
+            Assertions.assertEquals(3, list.get(2));
+        }
 
-    @Test
-    void fromTestIterableWithList() {
-        List<Integer> list = Streamable.from(List.of(1, 2, 3))
-                .toList();
-        Assertions.assertEquals(3, list.size());
-        Assertions.assertEquals(1, list.get(0));
-        Assertions.assertEquals(2, list.get(1));
-        Assertions.assertEquals(3, list.get(2));
-    }
+        @Test
+        void fromTestIterableWithStreamable() {
+            List<Integer> list = Streamable.from(Streamable.of(1, 2, 3))
+                    .parallel(3)
+                    .toList();
+            Assertions.assertEquals(3, list.size());
+            Assertions.assertEquals(1, list.get(0));
+            Assertions.assertEquals(2, list.get(1));
+            Assertions.assertEquals(3, list.get(2));
+        }
 
-    @Test
-    void fromTestIterableWithSet() {
-        List<Integer> list = Streamable.from(Set.of(1, 2, 3))
-                .toList();
-        Assertions.assertEquals(3, list.size());
-        Assertions.assertTrue(list.contains(1));
-        Assertions.assertTrue(list.contains(2));
-        Assertions.assertTrue(list.contains(3));
-    }
+        @Test
+        void fromTestIterableWithList() {
+            List<Integer> list = Streamable.from(List.of(1, 2, 3))
+                    .parallel(3)
+                    .toList();
+            Assertions.assertEquals(3, list.size());
+            Assertions.assertEquals(1, list.get(0));
+            Assertions.assertEquals(2, list.get(1));
+            Assertions.assertEquals(3, list.get(2));
+        }
 
-    @Test
-    void fromTestIterator() {
-        List<Integer> list = Streamable.from(new Iterator<Integer>() {
-            @Override
-            public boolean hasNext() {
-                return true;
-            }
+        @Test
+        void fromTestIterableWithSet() {
+            List<Integer> list = Streamable.from(Set.of(1, 2, 3))
+                    .parallel(3)
+                    .toList();
+            Assertions.assertEquals(3, list.size());
+            Assertions.assertTrue(list.contains(1));
+            Assertions.assertTrue(list.contains(2));
+            Assertions.assertTrue(list.contains(3));
+        }
 
-            @Override
-            public Integer next() {
-                return 0;
-            }
-        }).limit(3).toList();
-        Assertions.assertEquals(3, list.size());
-        Assertions.assertEquals(0, list.get(0));
-        Assertions.assertEquals(0, list.get(1));
-        Assertions.assertEquals(0, list.get(2));
+        @Test
+        void fromTestIterator() {
+            List<Integer> list = Streamable.from(new Iterator<Integer>() {
+                @Override
+                public boolean hasNext() {
+                    return true;
+                }
+
+                @Override
+                public Integer next() {
+                    return 0;
+                }
+            }).parallel(3).limit(3).toList();
+            Assertions.assertEquals(3, list.size());
+            Assertions.assertEquals(0, list.get(0));
+            Assertions.assertEquals(0, list.get(1));
+            Assertions.assertEquals(0, list.get(2));
+        }
     }
 }
