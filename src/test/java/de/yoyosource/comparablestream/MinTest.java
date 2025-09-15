70a8e6405214a803c3c33f614b5c85c99f018b67
yoyosource <yoyosource@nidido.de>
2025-09-15 08:12:40 +0200

    Add parallel tests

--- src/test/java/de/yoyosource/comparablestream/MinTest.java
+++ src/test/java/de/yoyosource/comparablestream/MinTest.java
@@ -3,26 +3,52 @@ package de.yoyosource.comparablestream;
 import de.yoyosource.streamable.Streamable;
 import de.yoyosource.streamable.streams.ComparableStream;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
 class MinTest {
 
-    @Test
-    void testMin() {
-        Optional<Integer> result = Streamable.of(1, 2, 3)
-                .as(ComparableStream.ComparableStream())
-                .min();
-        Assertions.assertTrue(result.isPresent());
-        Assertions.assertEquals(1, result.get());
+    @Nested
+    class Sequential {
+        @Test
+        void testMin() {
+            Optional<Integer> result = Streamable.of(1, 2, 3)
+                    .as(ComparableStream.ComparableStream())
+                    .min();
+            Assertions.assertTrue(result.isPresent());
+            Assertions.assertEquals(1, result.get());
+        }
+
+        @Test
+        void testMinNoElement() {
+            Optional<Integer> result = Streamable.<Integer>of()
+                    .as(ComparableStream.ComparableStream())
+                    .min();
+            Assertions.assertTrue(result.isEmpty());
+        }
     }
 
-    @Test
-    void testMinNoElement() {
-        Optional<Integer> result = Streamable.<Integer>of()
-                .as(ComparableStream.ComparableStream())
-                .min();
-        Assertions.assertTrue(result.isEmpty());
+    @Nested
+    class Parallel {
+        @Test
+        void testMin() {
+            Optional<Integer> result = Streamable.of(1, 2, 3)
+                    .parallel(3)
+                    .as(ComparableStream.ComparableStream())
+                    .min();
+            Assertions.assertTrue(result.isPresent());
+            Assertions.assertEquals(1, result.get());
+        }
+
+        @Test
+        void testMinNoElement() {
+            Optional<Integer> result = Streamable.<Integer>of()
+                    .parallel(3)
+                    .as(ComparableStream.ComparableStream())
+                    .min();
+            Assertions.assertTrue(result.isEmpty());
+        }
     }
 }
--- src/test/java/de/yoyosource/comparablestream/SortedTest.java
+++ src/test/java/de/yoyosource/comparablestream/SortedTest.java
@@ -4,22 +4,43 @@ import de.yoyosource.streamable.Streamable;
 import de.yoyosource.streamable.streams.ComparableStream;
 import de.yoyosource.streamable.streams.JavaStream;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 class SortedTest {
 
-    @Test
-    void testSorted() {
-        List<Integer> result = Streamable.of(3, 2, 1)
-                .as(ComparableStream.ComparableStream())
-                .sorted()
-                .as(JavaStream.JavaStream())
-                .toList();
-        Assertions.assertEquals(3, result.size());
-        Assertions.assertEquals(1, result.get(0));
-        Assertions.assertEquals(2, result.get(1));
-        Assertions.assertEquals(3, result.get(2));
+    @Nested
+    class Sequential {
+        @Test
+        void testSorted() {
+            List<Integer> result = Streamable.of(3, 2, 1)
+                    .as(ComparableStream.ComparableStream())
+                    .sorted()
+                    .as(JavaStream.JavaStream())
+                    .toList();
+            Assertions.assertEquals(3, result.size());
+            Assertions.assertEquals(1, result.get(0));
+            Assertions.assertEquals(2, result.get(1));
+            Assertions.assertEquals(3, result.get(2));
+        }
+    }
+
+    @Nested
+    class Parallel {
+        @Test
+        void testSorted() {
+            List<Integer> result = Streamable.of(3, 2, 1)
+                    .parallel(3)
+                    .as(ComparableStream.ComparableStream())
+                    .sorted()
+                    .as(JavaStream.JavaStream())
+                    .toList();
+            Assertions.assertEquals(3, result.size());
+            Assertions.assertEquals(1, result.get(0));
+            Assertions.assertEquals(2, result.get(1));
+            Assertions.assertEquals(3, result.get(2));
+        }
     }
 }
