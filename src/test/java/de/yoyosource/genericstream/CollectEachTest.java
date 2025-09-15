b9248e0f9623227e4bcef7deecab46aad2bf66ae
yoyosource <yoyosource@nidido.de>
2025-09-15 08:14:19 +0200

    Add parallel tests

--- src/test/java/de/yoyosource/genericstream/CollectEachTest.java
+++ src/test/java/de/yoyosource/genericstream/CollectEachTest.java
@@ -4,6 +4,7 @@ import de.yoyosource.streamable.Streamable;
 import de.yoyosource.streamable.streams.GenericStream;
 import de.yoyosource.streamable.streams.JavaStream;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -11,15 +12,34 @@ import java.util.stream.Collectors;
 
 class CollectEachTest {
 
-    @Test
-    void testCollectEach() {
-        List<Long> list = Streamable.of(List.of(1, 2, 3), Streamable.of(4, 5, 6))
-                .map(Streamable::from)
-                .as(GenericStream.GenericStream())
-                .collectEach(JavaStream::count)
-                .collect(Collectors.toList());
-        Assertions.assertEquals(2, list.size());
-        Assertions.assertEquals(3, list.get(0));
-        Assertions.assertEquals(3, list.get(1));
+    @Nested
+    class Sequential {
+        @Test
+        void testCollectEach() {
+            List<Long> list = Streamable.of(List.of(1, 2, 3), Streamable.of(4, 5, 6))
+                    .map(Streamable::from)
+                    .as(GenericStream.GenericStream())
+                    .collectEach(JavaStream::count)
+                    .collect(Collectors.toList());
+            Assertions.assertEquals(2, list.size());
+            Assertions.assertEquals(3, list.get(0));
+            Assertions.assertEquals(3, list.get(1));
+        }
+    }
+
+    @Nested
+    class Parallel {
+        @Test
+        void testCollectEach() {
+            List<Long> list = Streamable.of(List.of(1, 2, 3), Streamable.of(4, 5, 6))
+                    .parallel(3)
+                    .map(Streamable::from)
+                    .as(GenericStream.GenericStream())
+                    .collectEach(JavaStream::count)
+                    .collect(Collectors.toList());
+            Assertions.assertEquals(2, list.size());
+            Assertions.assertEquals(3, list.get(0));
+            Assertions.assertEquals(3, list.get(1));
+        }
     }
 }
